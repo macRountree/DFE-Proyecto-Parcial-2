0f8e6667e5ead47df59827dbb1e0bbd19d71cda7
Macrountree <a219206225@unison.mx>
2023-10-26 11:49:33 -0700

    add variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 import { fetchApi } from '../config/fetch';
-// !======================VARIABLES GLOBALES
+// !======================VARIABLES GLOBALEs
+const tituloInput = document.querySelector('#titulo'); //mascota es titulo
+const descripcionInput = document.querySelector('#descripcion'); //sintomas es descripcion
+const completadoToggle = document.querySelector('#completado'); //Toggle
+const tagSelect = document.querySelector('#tag');
+const fechaInput = document.querySelector('#fecha');
+const prioridadSelect = document.querySelector('#prioridad');
+
 // ! ===================UI
+const formulario = document.querySelector('#nueva-tarea'); //nueva cita = nueva tarea
+const contenedorCitas = document.querySelector('#tareas');
+
+let editando;
 // !===============CLASES
 
 // !instancias
