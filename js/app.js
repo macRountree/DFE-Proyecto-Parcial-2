75b400c3072e12e7ea26b143b13a918ea4ac1cca
Macrountree <a219206225@unison.mx>
2023-10-27 15:27:10 -0700

    tareas done

--- js/app.js
+++ js/app.js
@@ -17,20 +17,17 @@ const contenedorTarea = document.querySelector('#tareas');
 let editando;
 // !===============CLASES
 class Tareas {
-  constructor() {
-    this.tareas = [];
-  }
   async agregarTarea(tarea) {
-    await fetchApi.post(tarea);
-    console.log(this.tareas);
+    return await fetchApi.post(tarea);
   }
-  eliminarTarea(id) {
-    this.tareas = this.tareas.filter(tarea => tarea.id !== id);
+  async eliminarTarea(id) {
+    limpiarTareas();
+    await fetchApi.delete(id);
+    // this.tareas = this.tareas.filter(tarea => tarea.id !== id);
   }
-  edTarea(tareaActual) {
-    this.tareas = this.tareas.map(tarea =>
-      tarea.id === tareaActual.id ? tareaActual : tarea
-    );
+  async edTarea(id, tareaActual) {
+    limpiarTareas();
+    await fetchApi.put(id, tareaActual);
   }
 }
 class UI {
@@ -59,9 +56,8 @@ class UI {
   }
   //   Aplicamos destructuring desde el parentesis de los argumentso
   imprimirTareas(tareas) {
-    this.limpiarHTML();
-    console.log(tareas);
     tareas.forEach(tarea => {
+      console.log(tarea);
       const { title, description, tag, dueDate, priority, completed, id } =
         tarea;
       const divTareas = document.createElement('DIV');
@@ -69,22 +65,22 @@ class UI {
       divTareas.dataset.id = id;
 
       //   Scripting de los elementos
-      const tituloParrafo = document.createElement('h2');
+      const tituloParrafo = document.createElement('h3');
       tituloParrafo.classList.add('card-title', 'font-weight-bolder');
       tituloParrafo.textContent = title;
 
-      const descripcionParrafo = document.createElement('p');
-      descripcionParrafo.innerHTML = ` <span class="font-weight-bolder"> sintomas: </span> ${description} `;
-      const prioridadParrafo = document.createElement('p');
-      prioridadParrafo.innerHTML = ` <span class="font-weight-bolder">descripcion: </span> ${priority} `;
-      const CompletadoParrafo = document.createElement('p');
-      CompletadoParrafo.innerHTML = ` <span class="font-weight-bolder"> hora: </span> ${
+      const descripcionParrafo = document.createElement('h4');
+      descripcionParrafo.innerHTML = ` <span class="font-weight-bolder"> Descripción: </span> ${description} `;
+      const prioridadParrafo = document.createElement('h4');
+      prioridadParrafo.innerHTML = ` <span class="font-weight-bolder">Prioridad: </span> ${priority} `;
+      const CompletadoParrafo = document.createElement('h4');
+      CompletadoParrafo.innerHTML = ` <span class="font-weight-bolder"> Completado: </span> ${
         completed ? 'Completada' : 'Pendiente'
       } `;
-      const tagParrafo = document.createElement('p');
-      tagParrafo.innerHTML = ` <span class="font-weight-bolder">prioridad: </span> ${tag} `;
-      const fechaParrafo = document.createElement('p');
-      fechaParrafo.innerHTML = ` <span class="font-weight-bolder"> fecha: </span> ${dueDate} `;
+      const tagParrafo = document.createElement('h4');
+      tagParrafo.innerHTML = ` <span class="font-weight-bolder">Tag: </span> ${tag} `;
+      const fechaParrafo = document.createElement('h4');
+      fechaParrafo.innerHTML = ` <span class="font-weight-bolder"> Fecha: </span> ${dueDate} `;
       // BtnEliminar =
       const btnEliminar = document.createElement('button');
       btnEliminar.classList.add('btn', 'btn-outline-danger', 'mr-2');
@@ -103,7 +99,7 @@ class UI {
       btnEditar.innerHTML =
         'Editar Tarea <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>   ';
       btnEditar.onclick = () => {
-        editarTarea(tarea);
+        editarTarea(id);
       };
       // agregamos parrafos  al div tarea
       divTareas.appendChild(tituloParrafo);
@@ -163,13 +159,13 @@ function checkedTarea(e) {
   tareaObjeto[e.target.name] = e.target.checked;
 }
 
-function nuevatarea(e) {
+async function nuevatarea(e) {
   e.preventDefault();
   const { titulo, descripcion, tag, fecha, completado, prioridad } =
     tareaObjeto;
 
   console.log(tareaObjeto);
-
+  let recibirId = '';
   if (
     titulo === '' ||
     descripcion === '' ||
@@ -182,7 +178,11 @@ function nuevatarea(e) {
   }
   if (editando) {
     ui.imprimirAlerta('Tarea agregada correctamente');
-    administrarTareas.edTarea({
+    recibirId = this.querySelector('button[type="submit"]').getAttribute(
+      'data-id'
+    );
+    console.log(recibirId);
+    await administrarTareas.edTarea(recibirId, {
       title: titulo,
       description: descripcion,
       completed: completado,
@@ -191,10 +191,10 @@ function nuevatarea(e) {
       dueDate: fecha,
     });
     formulario.querySelector('button[type="submit"]').textContent =
-      'Crear Tarea';
+      'Añadir Tarea';
     editando = false;
   } else {
-    administrarTareas.agregarTarea({
+    const result = await administrarTareas.agregarTarea({
       title: titulo,
       description: descripcion,
       completed: completado,
@@ -202,13 +202,24 @@ function nuevatarea(e) {
       tag: tag,
       dueDate: fecha,
     });
+    recibirId = result.id;
     ui.imprimirAlerta('Tarea agregado correctamente');
   }
 
   reiniciarObjeto();
   formulario.reset();
 
-  ui.imprimirTareas(administrarTareas);
+  ui.imprimirTareas([
+    {
+      title: titulo,
+      description: descripcion,
+      completed: completado,
+      priority: prioridad,
+      tag: tag,
+      dueDate: fecha,
+      id: recibirId,
+    },
+  ]);
 }
 
 function reiniciarObjeto() {
@@ -220,33 +231,52 @@ function reiniciarObjeto() {
   tareaObjeto.fecha = '';
 }
 
-function eliminarTarea(id) {
+async function eliminarTarea(id) {
   // administrarTareas.eliminarTarea(id);
+
   console.log(id);
+  await administrarTareas.eliminarTarea(id);
+  const resultado = await fetchApi.getAll();
   ui.imprimirAlerta('La tarea se eliminó correctamente');
-  ui.imprimirTareas(administrarTareas);
+  ui.imprimirTareas(resultado);
 }
 
-function editarTarea(tarea) {
-  const { titulo, descripcion, prioridad, fecha, tag, completado, id } = tarea;
-  tituloInput.value = titulo;
-  descripcionInput.value = descripcion;
-  prioridadSelect.value = prioridad;
-  completadoToggle.value = completado;
-  fechaInput.value = fecha;
+async function editarTarea(tarea) {
+  const recibir = await fetchApi.getById(tarea);
+  console.log(recibir);
+  const { title, description, priority, dueDate, tag, completed } = recibir;
+  const traerToggle = document.getElementsByClassName('toggle')[0];
+
+  if (completed) {
+    traerToggle.className = 'toggle btn btn-success';
+  } else {
+    traerToggle.className = 'toggle btn btn-danger off';
+  }
+
+  tituloInput.value = title;
+  descripcionInput.value = description;
+  prioridadSelect.value = priority;
+  completadoToggle.checked = completed;
+  fechaInput.value = dueDate;
   tagSelect.value = tag;
   // llenamos el objeto
-  tareaObjeto.titulo = titulo;
-  tareaObjeto.descripcion = descripcion;
-  tareaObjeto.prioridad = prioridad;
-  tareaObjeto.completado = completado;
-  tareaObjeto.fecha = fecha;
+  tareaObjeto.titulo = title;
+  tareaObjeto.descripcion = description;
+  tareaObjeto.prioridad = priority;
+  tareaObjeto.completado = completed;
+  tareaObjeto.fecha = dueDate;
   tareaObjeto.tag = tag;
-  tareaObjeto.id = id;
 
   // Cambiar boton
   formulario.querySelector('button[type="submit"]').textContent =
     'Editar Tarea';
-
+  formulario
+    .querySelector('button[type="submit"]')
+    .setAttribute('data-id', recibir.id);
   editando = true;
+}
+
+function limpiarTareas() {
+  const tareas = document.getElementById('tareas');
+  tareas.innerHTML = '';
 }
